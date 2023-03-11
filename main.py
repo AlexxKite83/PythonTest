0b0adca285f4f72824080fd3c690633d4e8efffb
AlexxKite83 <alexey.megadom@gmail.com>
2023-03-11 22:26:20 +0200

    Finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from os import listdir
 import pygame
 from pygame. constants import QUIT,  K_DOWN, K_UP, K_LEFT, K_RIGHT
 
@@ -10,31 +11,48 @@ RED = (255, 0, 0)
 GREEN = (34, 177, 76)
 ball_color = WHITE
 
+font = pygame.font.SysFont('Verdana', 20)
+
 screen = width, height = 800,600
 main_surface = pygame.display.set_mode(screen)
-ball = pygame.Surface((20, 20))
-ball.fill(ball_color)
+# ball = pygame.Surface((20, 20))
+# ball.fill(ball_color)
+IMGS_PATH = 'goose'
+ball_imgs = [pygame.image.load(IMGS_PATH + '/' + file).convert_alpha() for file in listdir(IMGS_PATH)]
+ball = ball_imgs[0]
+# ball = pygame.image.load('player.png').convert_alpha()
 bal_rect = ball.get_rect()
-ball_speed = 5
+ball_speed = 10
 
 def create_enemy():
-    enemy = pygame.Surface((20, 20))
-    enemy.fill(RED)
-    enemy_rect = pygame.Rect(width, random.randint(0, height), *enemy.get_size())
+    enemy = pygame.image.load('enemy.png').convert_alpha()
+    # enemy = pygame.Surface((20, 20))
+    # enemy.fill(RED)
+    enemy_rect = pygame.Rect(width, random.randint(40, height-40), *enemy.get_size())
     enemy_speed = random.randint(2, 5)
     return [enemy, enemy_rect, enemy_speed]
 CREATE_EMEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(CREATE_EMEMY, 1500)
 
 def create_bonus():
-    bonus = pygame.Surface((20, 20))
-    bonus.fill(GREEN)
-    bonus_rect = pygame.Rect(random.randint(0, width), 0, *bonus.get_size())
+    bonus = pygame.image.load('bonus.png').convert_alpha()
+    # bonus = pygame.Surface((20, 20))
+    # bonus.fill(GREEN)
+    bonus_rect = pygame.Rect(random.randint(100, width-200), 0, *bonus.get_size())
     bonus_speed = random.randint(1, 4)
     return [bonus, bonus_rect, bonus_speed]
-CREATE_BONUS = pygame.USEREVENT + 1
+CREATE_BONUS = pygame.USEREVENT + 2
 pygame.time.set_timer(CREATE_BONUS, 1500)
 
+CHANGE_IMG = pygame.USEREVENT + 3
+pygame.time.set_timer(CHANGE_IMG, 125)
+
+bg = pygame.transform.scale(pygame.image.load('background.png').convert(), screen)
+bgX = 0
+bgX2 = bg.get_width()
+bg_speed = 3
+img_index = 0
+scores = 0
 enemies = []
 bonuses = []
 
@@ -48,20 +66,39 @@ while is_working:
             enemies.append(create_enemy())
         if event.type == CREATE_BONUS:
             bonuses.append(create_bonus())
-    
+        if event.type == CHANGE_IMG:
+            img_index += 1
+            if img_index == len(ball_imgs):
+                img_index = 0
+            ball = ball_imgs[img_index]
+            
     pressed_keys = pygame.key.get_pressed()
     
-    main_surface.fill(BLACK)
+    # main_surface.fill(WHITE)
+
+    # main_surface.blit(bg, (0, 0))
+    bgX -= bg_speed
+    bgX2 -= bg_speed 
+
+    if bgX < -bg.get_width():
+       bgX = bg.get_width()
+    if bgX2 < -bg.get_width():
+       bgX2 = bg.get_width() 
+        
+    main_surface.blit(bg, (bgX, 0))
+    main_surface.blit(bg, (bgX2, 0))
+
 
     main_surface.blit(ball, bal_rect)
 
+    main_surface.blit(font.render(str(scores), True, BLACK), (width-30, 0))
     for enemy in enemies:
         enemy[1] = enemy[1].move(-enemy[2], 0)
         main_surface.blit(enemy[0], enemy[1])
         if  enemy[1].left < 0:
             enemies.pop(enemies.index(enemy))
         if bal_rect.colliderect(enemy[1]):
-            enemies.pop(enemies.index(enemy))
+           is_working = False
 
     for bonus in bonuses:
         bonus[1] = bonus[1].move(0, bonus[2])
@@ -70,6 +107,7 @@ while is_working:
             bonuses.pop(bonuses.index(bonus))
         if bal_rect.colliderect(bonus[1]):
             bonuses.pop(bonuses.index(bonus))
+            scores = scores + 1
 
     if pressed_keys[K_DOWN] and bal_rect.bottom <= height:
         bal_rect = bal_rect.move((0, ball_speed))
@@ -81,9 +119,9 @@ while is_working:
         bal_rect = bal_rect.move((-ball_speed, 0))
     
     
-    print(len(bonuses))
+    # print(len(bonuses))
 
    # enemy_rect = enemy_rect.move(-enemy_speed, 0)
-    ball.fill(ball_color)
+    # ball.fill(ball_color)
     pygame.display.flip()
 
